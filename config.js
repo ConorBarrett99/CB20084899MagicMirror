2d518df4fceee44595b270fcff06063a58ea4e9c
ConorBarrett99 <47248218+ConorBarrett99@users.noreply.github.com>
2021-05-09 23:54:29 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,97 @@
+/* Magic Mirror Config Sample
+ *
+ * By Michael Teeuw https://michaelteeuw.nl
+ * MIT Licensed.
+ *
+ * For more information on how you can configure this file
+ * See https://github.com/MichMich/MagicMirror#configuration
+ *
+ */
+
+var config = {
+	address: "localhost", 	// Address to listen on, can be:
+							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+							// - another specific IPv4/6 to listen on a specific interface
+							// - "0.0.0.0", "::" to listen on any interface
+							// Default, when address config is left out or empty, is "localhost"
+	port: 8080,
+	basePath: "/", 	// The URL path where MagicMirror is hosted. If you are using a Reverse proxy
+					// you must set the sub path here. basePath must end with a /
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], 	// Set [] to allow all IP addresses
+															// or add a specific IPv4 of 192.168.1.5 :
+															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+															// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+
+	useHttps: false, 		// Support HTTPS or not, default "false" will use HTTP
+	httpsPrivateKey: "", 	// HTTPS private key path, only require when useHttps is true
+	httpsCertificate: "", 	// HTTPS Certificate path, only require when useHttps is true
+
+	language: "en",
+	locale: "en-US",
+	logLevel: ["INFO", "LOG", "WARN", "ERROR"], // Add "DEBUG" for even more logging
+	timeFormat: 24,
+	units: "metric",
+	// serverOnly:  true/false/"local" ,
+	// local for armv6l processors, default
+	//   starts serveronly and then starts chrome browser
+	// false, default for all NON-armv6l devices
+	// true, force serveronly mode, because you want to.. no UI on this device
+
+	modules: [
+		{
+			module: "alert",
+		},
+		{
+			module: "clock",
+			position: "top_left"
+		},
+		
+		{
+			module: "compliments",
+			position: "bottom_left"
+		},
+	
+		{
+			module: "MMM-cryptocurrency",
+			position: "top_right",
+			config: {
+				apikey: 'f203f60f-3e0a-4fab-82c6-b83b6a35cfe8',
+				currency: ['bitcoin'],
+				conversion: 'EUR',
+				headers: ['change24h', 'change1h', 'change7d'],
+				displayType: 'logoWithChanges',
+				showGraphs: true
+			}
+		},
+		{
+			module: "newsfeed",
+			position: "bottom_left",
+			config: {
+				feeds: [
+					{
+						title: "RTE NEWS",
+						url: "https://www.rte.ie/feeds/rss/?index=/news/&limit=20"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true,
+				broadcastNewsFeeds: true,
+				broadcastNewsUpdates: true
+			}
+		},
+		{
+    			module: 'MMM-WiFiPassword',
+    			position: "bottom_right",
+      			config: {
+        			//See 'Configuration options' for more information.
+        			network: "eir89369693", 
+        			password: "k9pK4RTbeh",
+      		}
+  		},
+	
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {module.exports = config;}
